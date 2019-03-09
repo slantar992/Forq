1b1a4597ecdf671c850cd052d0a9ac894d70cada
Alberto Martin <albertouma92@gmail.com>
2019-03-09 18:48:51 +0100

    GroupBy implemented

---
+++ src/Classes/Group.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Slantar.Forq
+{
+    public class Group<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        private TKey key = default(TKey);
+        private List<TElement> elements = new List<TElement>();
+
+        public TKey Key { get { return key; } }
+        public List<TElement> Elements { get { return elements; } }
+
+        public Group(TKey key)
+        {
+            this.key = key;
+        }
+
+        public Group(TKey key, List<TElement> elements = null) : this(key)
+        {
+            if (elements != null)
+            {
+                elements.AddRange(elements);
+            }
+        }
+    }
+}
---
+++ src/Interfaces/IGrouping.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Slantar.Forq
+{
+    public interface IGrouping<TKey, TElement>
+    {
+        TKey Key { get; }
+        List<TElement> Elements { get; }
+    }
+}
+
